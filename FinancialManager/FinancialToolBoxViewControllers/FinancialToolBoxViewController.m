6385bfce38b98f366c867930f6c031561c74f490
Zoe <705152822@qq.com>
2013-03-28 17:29:57 +0800

    计算器

--- FinancialManager/FinancialToolBoxViewControllers/FinancialToolBoxViewController.m
+++ FinancialManager/FinancialToolBoxViewControllers/FinancialToolBoxViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "FinancialToolBoxViewController.h"
+#import "Cal1_1ViewController.h"
 
 @interface FinancialToolBoxViewController ()
 
@@ -26,15 +27,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 200, 200)];
-    label.text=@"金融工具箱";
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 120, 200, 200)];
+    label.text=@"计算器";
     label.font=[UIFont systemFontOfSize:20];
     [self.view addSubview:label];
     
-    
-    
-    
-    
+    UIButton *calculatorBtn =[UIButton buttonWithType:UIButtonTypeCustom];
+    [calculatorBtn setBackgroundImage:[UIImage imageNamed:@"icon1.0.png"] forState:UIControlStateNormal];
+    calculatorBtn.frame=CGRectMake(100, 100, 100, 100);
+    [calculatorBtn addTarget:self action:@selector(calculatorClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:calculatorBtn];
+        
+}
+
+- (void)calculatorClick:(id)sender{
+    Cal1_1ViewController *calVC = [[Cal1_1ViewController alloc] init];
+    [self.navigationController pushViewController:calVC animated:YES];
+
 }
 
 - (void)didReceiveMemoryWarning
